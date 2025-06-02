d62989eb7b44cf542fc149b8738d928dc49f01c9
shohan12727 <aishohan001@gmail.com>
2025-06-02 13:50:33 +0600

    while problem

---
+++ 18.js
@@ -0,0 +1,56 @@
+// page: 111 
+
+//  let num2 = 5;
+// let sum = 0;
+// while (num2 < 15) {
+//   sum += num2;
+//   num2++;
+// }
+// console.log(sum); // Outputs: 95
+
+
+let num = 50;
+while (num <= 100) {
+  console.log(num);
+  num++
+}
+
+let num2 = 5;
+let sum = 0;
+while (num2 <= 15) {
+
+  sum = sum + num2;
+  num2++
+}
+console.log(sum);
+
+
+let roll = 1;
+let addition = 0;
+while (roll <= 50) {
+  addition += roll;
+  roll++
+}
+console.log(addition);
+
+
+
+let num7 = 21;
+let addition7 = 0;
+
+while (num7 <= 50) {
+  addition7 += num7;
+  console.log(addition7);
+  console.log(num7);
+  num7++;
+}
+
+
+
+let num8 = 20; 
+let addition8 = 0;
+while(num8 <= 40) {
+  addition8 += num8 ;
+  num8++
+}
+console.log(addition8);
