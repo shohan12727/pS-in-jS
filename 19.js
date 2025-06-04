d0eb45ec63e030f6c6a86ec853971bfdb8cfe765
shohan12727 <aishohan001@gmail.com>
2025-06-04 11:16:27 +0600

    page 116 done

---
+++ 19.js
@@ -0,0 +1,19 @@
+// page: 114 
+for (let num = 150; num < 170; num++) {
+  // console.log(num);
+}
+
+let sum = 0;
+
+for (let roll = 31; roll <= 58; roll++) {
+  sum += roll;
+
+}
+console.log("sum of 31 to 58 ", sum);
+
+
+let sum2 = 0;
+for (let num2 = 25; num2 <= 75; num2++) {
+  sum2 += num2;
+}
+console.log(sum2);
---
+++ 20.js
@@ -0,0 +1,52 @@
+// page: 119
+for (let i = 20; i <= 50; i++) {
+  if (i % 7 == 0) {
+    // console.log(i);
+
+  }
+}
+
+for (let j = 40; j <= 80; j++) {
+  if (j % 5 == 0 && j % 7 == 0) {
+    // console.log(j);
+
+  }
+}
+
+
+
+
+let sum = 0;
+for (let k = 1; k <= 40; k++) {
+  if (k % 13 == 0) {
+    sum += k;
+  }
+}
+// console.log("sum of all divisal number", sum);
+
+
+
+for (let x = 1; x <= 50; x += 4) {
+  // console.log(x);
+}
+
+
+
+for (let m = 0; m <= 100; m++) {
+  if (m % 9 == 0 && m % 6 == 0) {
+    // console.log(m);
+  }
+}
+
+
+
+let addition = 0;
+for (let o = 1; o <= 50; o++) {
+  if (o % 3 == 0 && o % 4 == 0) {
+    addition += o;
+
+  }
+}
+
+console.log("additon:", addition);
+
