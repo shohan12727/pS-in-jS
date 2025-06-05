e331a8932b904d2afc7cbbaf0f1c066b061b10b5
shohan12727 <aishohan001@gmail.com>
2025-06-05 14:57:36 +0600

    page 122 done

---
+++ 21.js
@@ -0,0 +1,49 @@
+// page 122
+for (let i = 1; i <= 30; i = i + 1) {
+  if (i === 15) {
+    break;
+  }
+  console.log(i);
+
+}
+
+
+for (let j = 1; j <= 40; j++) {
+  if (j % 7 == 0) {
+    continue
+  }
+  console.log(j);
+
+}
+
+for (let k = 1; k <= 15; k++) {
+  if (k == 9) {
+    continue
+  }
+  console.log(k);
+
+}
+
+
+for (let m = 1; m <= 20; m++) {
+  if (m == 12) {
+    continue
+  }
+  console.log(m);
+
+}
+
+for (let n = 1; n <= 25; n++) {
+  if (n % 3 == 0) {
+    continue
+  }
+  console.log(n);
+}
+
+for( let x = 91; x <= 120 ; x++){
+  if(x % 10 ==0){
+    break
+  }
+  console.log(x);
+  
+}
