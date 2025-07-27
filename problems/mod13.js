b287418284dcc0482288fee319ecc4d4c34e1c1e
shohan12727 <aishohan001@gmail.com>
2025-07-28 00:57:27 +0600

    od13 added

---
+++ problems/mod13.js
@@ -0,0 +1,21 @@
+const bangla = 85;
+const english = 98;
+const math = 80;
+
+const avarage = (bangla + english + math)/3 ;
+console.log(avarage);
+
+
+const num = 119;
+const reminder = num % 5;
+console.log(reminder);
+
+
+
+ 
+const a = isNaN("120");
+const b = isNaN(12-8);
+
+console.log(a,b);
+
+
